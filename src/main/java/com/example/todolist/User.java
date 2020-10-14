16bfca5debb98778569e8c0c488854769e0676a6
maks99km <kanapyanov-m@mail.ru>
2020-10-14 13:08:15 +0600

    added page's

--- src/main/java/com/example/todolist/User.java
+++ src/main/java/com/example/todolist/User.java
@@ -13,13 +13,27 @@ public class User {
 
     private String name;
     private String email;
-    public  Integer getId() { return  id;}
-    public void setId(Integer id){ this.id = id;}
 
-    public  String getName() { return  name;}
-    public void setName(String name){ this.name = name;}
+    public  Integer getId() {
+        return  id;
+    }
 
-    public  String getEmail() { return  email;}
-    public void setEmail(String email){ this.email = email;}
+    public void setId(Integer id){
+        this.id = id;
+    }
+
+    public  String getName() {
+        return  name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public  String getEmail() {
+        return  email;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
 
 }
